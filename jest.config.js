49110c9ebf40a397609c30fa3cdc527db8670d7d
Vriendt <thijs.vandegriendt@xs4all.nl>
2026-05-04 17:01:33 +0200

    setup

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testEnvironment: '@oaklean/profiler-jest-environment/env.js',
+  globalSetup: '@oaklean/profiler-jest-environment/setup.js',
+  globalTeardown: '@oaklean/profiler-jest-environment/teardown.js',
+}
---
+++ microchannel.test.js
@@ -0,0 +1,67 @@
+const CPM = require('./artistoo/build/artistoo-cjs.js')
+
+let config = {
+  ndim: 2,
+  field_size: [300, 15],
+  conf: {
+    torus: [true, false],
+    seed: 1,
+    T: 20,
+    J: [[NaN, 20], [20, 5]],
+    LAMBDA_V: [0, 30],
+    V: [0, 500],
+    LAMBDA_P: [0, 2],
+    P: [0, 300],
+    LAMBDA_ACT: [0, 500],
+    MAX_ACT: [0, 40],
+    ACT_MEAN: "geometric"
+  },
+  simsettings: {
+    NRCELLS: [3],
+    BURNIN: 100,
+    RUNTIME: 1000,
+    RUNTIME_BROWSER: "Inf",
+    STATSOUT: { browser: false, node: true },
+    LOGRATE: 10,
+    SAVEIMG: false  // disable image saving in tests
+  }
+}
+
+let custommethods = {
+  initializeGrid,
+  buildChannel,
+  drawBelow
+}
+
+function drawBelow() {
+  this.Cim.drawPixelSet(this.channelvoxels, "AAAAAA")
+}
+
+function initializeGrid() {
+  if (!this.helpClasses["gm"]) { this.addGridManipulator() }
+  let nrcells = this.conf["NRCELLS"], cellkind, i
+  this.buildChannel()
+  for (cellkind = 0; cellkind < nrcells.length; cellkind++) {
+    for (i = 0; i < nrcells[cellkind]; i++) {
+      if (i == 0) {
+        this.gm.seedCellAt(cellkind + 1, this.C.midpoint)
+      } else {
+        this.gm.seedCell(cellkind + 1)
+      }
+    }
+  }
+}
+
+function buildChannel() {
+  this.channelvoxels = this.gm.makePlane([], 1, 0)
+  let gridheight = this.C.extents[1]
+  this.channelvoxels = this.gm.makePlane(this.channelvoxels, 1, gridheight - 1)
+  this.C.add(new CPM.BorderConstraint({
+    BARRIER_VOXELS: this.channelvoxels
+  }))
+}
+
+test('Microchannel simulation runs without errors', () => {
+  const sim = new CPM.Simulation(config, custommethods)
+  expect(() => sim.run()).not.toThrow()
+})
